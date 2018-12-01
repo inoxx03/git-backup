461f14ccbde687be712f964a807dbe70e6531428
Stefan Sitani <ssitani@redhat.com>
2018-12-01 23:32:41 +0100

    Add git remote URL scrape script

---
+++ get_git_remote_urls.sh
@@ -0,0 +1,38 @@
+# /bin/bash
+
+DIR_LIST=${@}
+
+# DO NOT include trailing slash in git directory home path
+# TODO: Move git path and output file into separate properties file to externalize configuration
+GIT_HOME_PATH="/home/ssitani/git_repos"
+
+OUTPUT_FILE_NAME="../remotelist-test1.txt"
+
+for dir in $DIR_LIST
+do
+
+        if [ -d ${GIT_HOME_PATH}/${dir} ] ; then
+
+            dir_name="${dir}"
+
+            cd ${GIT_HOME_PATH}/${dir_name}
+
+            #print the name of the current repo directory
+            #echo "[DEBUG]: $(dir_name)"
+
+            echo "# ${dir_name}" >> ${OUTPUT_FILE_NAME}
+
+            # print the matched entry
+            #echo "[DEBUG]: $(git remote -v | egrep -io ".*[[:space:]](git@git).*(.git)")"
+
+            git remote -v | egrep -io ".*[[:space:]](git@git).*(.git)" >> ${OUTPUT_FILE_NAME} || git remote -v | egrep -io ".*[[:space:]](https://).*(.git)" >> ${OUTPUT_FILE_NAME}
+
+            echo " " >> ${OUTPUT_FILE_NAME}
+
+            cd ${GIT_HOME_PATH}
+
+        fi
+done
+
+
+exit 0
