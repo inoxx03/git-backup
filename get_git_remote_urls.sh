34343b6bc58ccd90fc2a7abf548f4e95123b4de6
Stefan Sitani <ssitani@redhat.com>
2018-12-02 01:00:02 +0100

    hardcoce output file path, change output file name

--- get_git_remote_urls.sh
+++ get_git_remote_urls.sh
@@ -1,12 +1,18 @@
 # /bin/bash
 
+# Takes one or mmore directories as arguments
 DIR_LIST=${@}
 
 # DO NOT include trailing slash in git directory home path
 # TODO: Move git path and output file into separate properties file to externalize configuration
 GIT_HOME_PATH="/home/ssitani/git_repos"
 
-OUTPUT_FILE_NAME="../remotelist-test1.txt"
+# name of, and path to, the output file, currently hard-coded
+
+WORKING_DIR=${pwd}
+#OUTPUT_FILE_NAME="remotelist-test1.txt"
+OUTPUT_FILE_PATH="../remotelist-export_2018-12-02.txt"
+# OUTPUT_FILE_PATH="${pwd}/${OUTPUT_FILE_NAME}"
 
 for dir in $DIR_LIST
 do
@@ -20,14 +26,14 @@ do
             #print the name of the current repo directory
             #echo "[DEBUG]: $(dir_name)"
 
-            echo "# ${dir_name}" >> ${OUTPUT_FILE_NAME}
+            echo "# ${dir_name}" >> ${OUTPUT_FILE_PATH}
 
             # print the matched entry
             #echo "[DEBUG]: $(git remote -v | egrep -io ".*[[:space:]](git@git).*(.git)")"
 
-            git remote -v | egrep -io ".*(.git)" >> ${OUTPUT_FILE_NAME}
+            git remote -v | egrep -io ".*(.git)" >> ${OUTPUT_FILE_PATH}
 
-            echo " " >> ${OUTPUT_FILE_NAME}
+            echo " " >> ${OUTPUT_FILE_PATH}
 
             cd ${GIT_HOME_PATH}
 
